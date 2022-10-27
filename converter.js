b4a236b1eb6370e967f14eb01ea1d4b2b48d2244
jaredshedr <jaredsurfside@gmail.com>
2022-10-27 12:25:41 -0400

    small c

--- converter.js
+++ converter.js
@@ -1,18 +1,17 @@
-module.exports.getTime = function(APIResult) {
-  const secondsToTime = function(timeInSeconds) {
+module.exports.getTime = function (APIResult) {
+  const secondsToTime = function (timeInSeconds) {
     const timeInMinutes = Math.floor(timeInSeconds / 60);
     const numberOfMinutes = timeInMinutes % 60;
     const numberOfHours = Math.floor(timeInMinutes / 60);
-    const hourText = numberOfHours === 1 ? 'hour' : 'hours';
-    const minuteText = numberOfMinutes === 1 ? 'minute' : 'minutes';
+    const hourText = numberOfHours === 1 ? "hour" : "hours";
+    const minuteText = numberOfMinutes === 1 ? "minute" : "minutes";
     return `Time to destination: ${numberOfHours} ${hourText} and ${numberOfMinutes} ${minuteText}`;
-  }
-  const getNumberOfSeconds = function(returnTime) {
+  };
+  const getNumberOfSeconds = function (returnTime) {
     delete returnTime[returnTime.length - 1];
     return parseInt(returnTime);
-  }
+  };
 
   const timeInSeconds = getNumberOfSeconds(APIResult);
   return secondsToTime(timeInSeconds);
-
-}
+};
