9dc5701ed96c5223c277e32c461fa5f8454c665f
jaredshedr <jaredsurfside@gmail.com>
2023-11-21 11:42:52 -0500

    change

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 
 const port = 3000;
 
-let getDuration = schedule.scheduleJob("30 11 * * MON-FRI", async () => {
+let getDuration = schedule.scheduleJob("0 12 * * MON-FRI", async () => {
 
   let catUrl = 'https://catfact.ninja/fact?max_length=140'
   let catFact;
